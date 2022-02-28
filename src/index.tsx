a423a12f397a8a408fb98ae05eb1cc224ca18154
Daniel Becker <danielbeckeralves@gmail.com>
2022-02-28 13:10:10 -0300

    ajustado transactions do miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -13,19 +13,11 @@ createServer({
             transactions: [
                 {
                     id: 1,
-                    title: 'Freelance de website',
+                    title: 'Salário',
                     type: 'deposit',
                     category: 'Dev',
                     amount: 6000,
                     createdAt: new Date('2021-02-12 09:00:00'),
-                },
-                {
-                    id: 2,
-                    title: 'Aluguel',
-                    type: 'withdraw',
-                    category: 'Casa',
-                    amount: 1100,
-                    createdAt: new Date('2021-02-14 11:00:00'),
                 },
             ],
         })
